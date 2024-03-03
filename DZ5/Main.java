1e2a4e0b2a80af440a0b712eb614a7757087bc32
Anna Oleksevich <oleksanna@mail.ru>
2024-03-03 23:29:05 +0800

    Домашнее задание №5 дополнение

--- DZ5/Main.java
+++ DZ5/Main.java
@@ -13,30 +13,30 @@ public class Main {
         Controller controller = new Controller();
 
         // Создаем преподавателей
-        Teacher teacher1 = new Teacher("Иван", "Иванов", "Иванович", 1);
-        Teacher teacher2 = new Teacher("Петр", "Петров", "Петрович", 2);
+        Teacher teacher1 = new Teacher("Сидор", "Матрасович", "Пупкин", 1);
+        Teacher teacher2 = new Teacher("Максим", "Петрович", "Зайкин", 2);
 
         // Создаем студентов
-        controller.createStudent("Студент 1", "Иванов", "Иван", 1);
-        controller.createStudent("Студент 2", "Петров", "Петр", 2);
-        controller.createStudent("Студент 3", "Семенов", "Семен", 3);
-        controller.createStudent("Студент 4", "Романов", "Роман", 4);
-        controller.createStudent("Студент 5", "Васильев", "Василий", 5);
+        controller.createStudent("Студент 1", "Иван", "Иванов", 1);
+        controller.createStudent("Студент 2", "Петр", "Петров", 2);
+        controller.createStudent("Студент 3", "Семен", "Семенов", 3);
+        controller.createStudent("Студент 4", "Роман", "Романов", 4);
+        controller.createStudent("Студент 5", "Василий", "Васильев", 5);
 
         // Получаем список идентификаторов студентов
         List<Integer> studentIds = controller.getAllStudentIds();
 
         // Формируем учебные группы
         List<Student> students1 = new ArrayList<>();
-        students1.add(new Student("Студент 1", "Иванов", "Иван", 1));
-        students1.add(new Student("Студент 2", "Петров", "Петр", 2));
+        students1.add(new Student("Студент 1", "Иван", "Иванов", 1));
+        students1.add(new Student("Студент 2", "Петр", "Петров", 2));
         StudyGroup studyGroup1 = controller.formStudyGroup(teacher1, students1);
         System.out.println(studyGroup1);
 
         List<Student> students2 = new ArrayList<>();
-        students2.add(new Student("Студент 3", "Семенов", "Семен", 3));
-        students2.add(new Student("Студент 4", "Рлманов", "Роман", 4));
-        students2.add(new Student("Студент 5", "Васильев", "Василий", 5));
+        students2.add(new Student("Студент 3", "Семен", "Семенов", 3));
+        students2.add(new Student("Студент 4", "Роман", "Романов", 4));
+        students2.add(new Student("Студент 5", "Василий", "Васильев", 5));
         StudyGroup studyGroup2 = controller.formStudyGroup(teacher2, students2);
         System.out.println(studyGroup2);
     }
